a27ccab89f14d04b553fca677d7bebaa997cede3
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2026-02-19 23:01:43 +0530

    created initial structure and added dependencies

---
+++ src/main/java/dev/anuradha/omsinventoryservice/OmsInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.anuradha.omsinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmsInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/anuradha/omsinventoryservice/OmsInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anuradha.omsinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
